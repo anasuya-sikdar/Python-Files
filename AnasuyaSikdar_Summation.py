be1885de29712c9706695beb848eacaccb5df9c4
Anasuya Sikdar <136375229+anasuya-sikdar@users.noreply.github.com>
2023-12-21 15:48:06 +0530

    Update and rename AnasuyaSikdar_hw5_1.py to AnasuyaSikdar_Summation.py

---
+++ AnasuyaSikdar_Summation.py
@@ -0,0 +1,11 @@
+#Anasuya Sikdar
+#Assignment 5.1
+
+def evenRow(lst):
+    for i in range(len(lst)):  
+        sumNum = 0  
+        for j in range(len(lst[i])):  
+            sumNum += lst[i][j]  
+        if sumNum % 2 != 0:  
+            return False  
+    return True 
